e4edc2441a8d00c46713a8026735a7867723f7eb
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-05 21:39:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ def run_client():
   client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   try:
     client_socket.connect(('172.168.3.95', 5000))  # Asegúrate de que esta IP sea la correcta para el servidor
+
+    with mss.mss() as sct:
